a1ea912b2425db75fa292422945df28f00adb7a7
Balint Antal <balint@denv.io>
2023-12-03 19:37:39 +0100

    feat(day_03): cleanup implementation

--- day_03/main.hs
+++ day_03/main.hs
@@ -1,70 +1,109 @@
+import Data.Char
+import Data.List
+import System.Environment
 import System.IO
-import Data.Char (isDigit)
-import Data.List (elemIndices)
 
-processFile :: FilePath -> IO Int
--- processFile :: FilePath -> IO [Int]
-processFile filePath = do
-  handle <- openFile filePath ReadMode
-  result <- processFileContent handle [] 0
-  hClose handle
-  return result
+ignoredAdjecents = "1234567890."
+validFlags       = ["-v1", "-v2"]
 
-processFileContent :: Handle -> [String] -> Int -> IO Int
-processFileContent handle lines carry
-  -- INIT (List has incorrect length.)
+--- App Init ---
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case processArguments args of
+    Left errorMessage -> putStrLn $ "Error: " ++ errorMessage
+    Right (inputPath, processFunc) -> do
+      result <- processFile inputPath processFunc
+      putStrLn $ "Result: " ++ show result
+
+processArguments :: [String] -> Either String (String, [String] -> Int)
+processArguments args
+  | null inputPath               = Left "Must specify one input file"
+  | not (null invalidInputPaths) = Left "Multiple input paths specified"
+  | not (null invalidFlags)      = Left "Invalid flags provided"
+  | otherwise                    = Right (inputPath, processFunc)
+  where
+    inputPaths        = filter (\arg -> head arg /= '-') args
+    invalidInputPaths = drop 1 inputPaths
+    [inputPath]       = inputPaths
+    flagArgs          = filter (\arg -> head arg == '-') args
+    invalidFlags      = filter (`notElem` validFlags) flagArgs
+    processFunc
+      | "-v1" `elem` flagArgs = processFuncPart1
+      | "-v2" `elem` flagArgs = processFuncPart2
+      | otherwise             = processFuncPart2
+
+--- Handling file reading, and the processing of it in shifting chunks of 3 lines ---
+
+processFile :: FilePath -> ([String] -> Int) -> IO Int
+processFile filePath processFunc =
+  withFile filePath ReadMode $ \handle ->
+    processFileContent handle processFunc [] 0
+
+processFileContent :: Handle -> ([String] -> Int) -> [String] -> Int -> IO Int
+processFileContent handle processFunc lines carry
   | length lines /= 3 = do
     line_1 <- hGetLine handle
     line_2 <- hGetLine handle
-    processFileContent handle ["", line_1, line_2] carry
-  -- FINISH (Current line is not set.)
-  | lines !! 1 == "" = return carry
-  -- PROCESS
+    processFileContent handle processFunc ["", line_1, line_2] carry
+  | currentLine == "" = return carry
   | otherwise = do
-    -- let result = carry + sum (processLinesPart1 lines)
-    let result = carry + processLinesPart2 lines
-    hasReachedEndOfFile <- hIsEOF handle
-    if hasReachedEndOfFile then do
-      processFileContent handle [lines !! 1, lines !! 2, ""] result
-    else do
-      newLine <- hGetLine handle
-      let newLines = [lines !! 1, lines !! 2, newLine]
-      processFileContent handle newLines result
+      hasReachedEndOfFile <- hIsEOF handle
+      let getNextLines = if hasReachedEndOfFile
+          then return [currentLine, nextLine, ""]
+          else do
+            newLine <- hGetLine handle
+            return [currentLine, nextLine, newLine]
+      newLines <- getNextLines
+      processFileContent handle processFunc newLines result
+  where
+    result = carry + processFunc lines
+    [_, currentLine, nextLine] = lines
 
-processLinesPart1 :: [String] -> [Int]
-processLinesPart1 lines =
-  map (\(start, end) ->                                           -- 4.0: Parse the numbers from the "line" for the remaining indexes.  
-    read $                                                        --  .2: Parse the string into an Int.
-    substring (start, end) (lines !! 1)                           --  .1: Get the string on the position of the current line.
-  ) .
-  filter (\(start, end) ->                                        -- 3.0 - Filter the ones that do not have an adjecent special character.
-    any (any (`notElem` "1234567890.") .                          --  .2: Confirm if there's any non-blacklisted character.
-    substring (start - 1, end + 1)) lines                         --  .1: Get the string on the position and the surrounding items.
-  ) .
-  convertToRanges $                                               -- 2.0 - Convert them to ranges: [(0,0), (2,3), (5,7)]
-  [index | (char, index) <- zip (lines !! 1) [0..], isDigit char] -- 1.0 - Find indexes of numeric digits: [0, 2, 3, 5, 6, 7]
+--- Part 1 Implementation ---
 
-processLinesPart2 :: [String] -> Int
-processLinesPart2 lines = do
-  let numberRanges =
-        map (\line ->
-          convertToRanges [index | (char, index) <- zip line [0..], isDigit char]
-        ) lines
-  let gearNumbers =
-        filter (\numbers -> length numbers == 2) $
-        map (\position ->
-          concat $
-          zipWith (\ranges index ->
-            map (\(start, end) -> read (substring (start, end) (lines !! index)) :: Int) .
-            filter (\(start, end) -> start <= position + 1 && end >= position - 1) $
-            ranges
-          ) numberRanges [0..]
-        ) (elemIndices '*' $ lines !! 1)
-  sum (map product gearNumbers)
+processFuncPart1 :: [String] -> Int
+processFuncPart1 lines =
+  sum $
+    map (\(start, end) -> read (substring (start, end) currentLine)) .
+    filter (\(start, end) ->
+      any (any (`notElem` ignoredAdjecents) .
+      substring (start - 1, end + 1)) lines
+    ) $
+    numberRanges
+  where
+    currentLine         = lines !! 1
+    numberRanges        = getRangesOfConsecutiveNumbers $ getDigitIndices currentLine
+    getDigitIndices str = [index | (char, index) <- zip str [0..], isDigit char]
 
-convertToRanges :: [Int] -> [(Int, Int)]
-convertToRanges [] = []
-convertToRanges (x:xs) = go x x xs
+--- Part 2 Implementation ---
+
+processFuncPart2 :: [String] -> Int
+processFuncPart2 lines = sum (map product gearNumbers)
+  where
+    currentLine = lines !! 1
+    numberRanges =
+      map (\line ->
+        getRangesOfConsecutiveNumbers $
+        [index | (char, index) <- zip line [0..], isDigit char]
+      ) lines
+    gearNumbers =
+      filter (\numbers -> length numbers == 2) $
+      map (\position ->
+        concat $
+        zipWith (\ranges index ->
+          map (\(start, end) -> read (substring (start, end) (lines !! index)) :: Int) .
+          filter (\(start, end) -> start <= position + 1 && end >= position - 1) $
+          ranges
+        ) numberRanges [0..]
+      ) (elemIndices '*' currentLine)
+
+--- Utility Functions ---
+
+getRangesOfConsecutiveNumbers :: [Int] -> [(Int, Int)]
+getRangesOfConsecutiveNumbers [] = []
+getRangesOfConsecutiveNumbers (x:xs) = go x x xs
   where
     go start prev (y:ys)
       | y == prev + 1 = go start y ys
@@ -72,11 +111,4 @@ convertToRanges (x:xs) = go x x xs
     go start prev [] = [(start, prev)]
 
 substring :: (Int, Int) -> String -> String
-substring (start, end) str = drop start (take (end + 1) str)
-
-main :: IO ()
-main = do
-  result <- processFile "input.txt"
-  putStr "Part 1: "
-  putStr (show result)
-  putStr "\n"
+substring (start, end) = take (end - start + 1) . drop start
